37dd657da76f2cc2632b5c8ccd6a66ec590555ed
George_wen <wenyuan@huiwen.com>
2016-04-09 09:16:18 +0800

    Initial Commit

---
+++ WXMovie85/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WXMovie85
+//
+//  Created by wenyuan on 4/9/16.
+//  Copyright © 2016 wenyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WXMovie85/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WXMovie85
+//
+//  Created by wenyuan on 4/9/16.
+//  Copyright © 2016 wenyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
